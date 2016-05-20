42ce6187f5cfe95dcb08be7a59939d730d3420d6
Martin Kielhorn <martin.kielhorn@ezono.com>
2016-05-20 14:13:28 +0200

    add link to Greg Kroah-Hartman article

--- native-usb.lisp
+++ native-usb.lisp
@@ -1,4 +1,17 @@
-(mapcar #'(lambda (fn)
-	    (with-open-file (s fn)
-	      (read-line s)))
-	(directory "/sys/bus/usb/devices/usb*/*/idVendor"))
+(defun open-usb (vendor-id &optional (product-id 0))
+  (declare (type (unsigned-byte 16) vendor-id product-id))
+  (let* ((vendor-files (directory "/sys/bus/usb/devices/usb*/*/idVendor"))
+	 (vendors (mapcar #'(lambda (fn)
+			      (with-open-file (s fn)
+				(parse-integer (read-line s) :radix 16)))
+			  vendor-files)))
+    ;; FIXME handle multiple devices with same vendor-id
+    (elt vendor-files (position vendor-id vendors))))
+
+
+
+#+nil
+(open-usb #x10c4 #x87a0)
+
+
+;; https://www.linuxjournal.com/article/7466
